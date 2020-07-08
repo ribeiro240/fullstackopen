a86283de81a8d81d9023b01d7acf3cdedbe82b74
Filipe Ribeiro <filipepaiva240@gmail.com>
2020-07-07 23:53:42 -0300

    Implement search funcionality and refactor addName function

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import Header from './Components/Header';
+import './App.css';
 
 const App = (props) => {
-  const [namesList, setNamesList] = useState([]);
   const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [phonebook, setPhonebook] = useState([[name, number]]);
+  const [search, setSearch] = useState('');
 
   const addName = (event) => {
     event.preventDefault();
-    if (name.trim().length > 0) {
-      setNamesList([...namesList].concat(name.trim()));
+    let phonebookString = JSON.stringify(phonebook);
+    let numberNameString = JSON.stringify([name, number]);
+    let isRepeated = phonebookString.indexOf(numberNameString);
+    if (isRepeated === -1 && name.trim().length > 0) {
+      setPhonebook([...phonebook].concat([[name, number]]));
       setName('');
+      setNumber('');
+    } else {
+      alert(`${name} is already added to phonebook`)
     }
   }
 
@@ -17,19 +26,40 @@ const App = (props) => {
     setName(event.target.value);
   }
 
+  const handleNewNumber = (event) => {
+    setNumber(event.target.value);
+  }
+
+  const filteredPhonebook = phonebook.filter((phoneEntry) => {
+    return (phoneEntry[0].indexOf(search) !== -1) || (phoneEntry[1].indexOf(search) !== -1)
+  })
+
   return (
     <main>
-      <Header text="Phonebook" />
-      <ul>
-        {namesList.map((listItem, i) => {
-          return <li key={listItem + i}>{listItem}</li>
-        })}
-      </ul>
+      <Header text="Phonebook"/>
+      <input value={search}
+        onChange={(event) => setSearch(event.target.value)}
+       />
+      <Header text="Add a new Name"/>
       <form onSubmit={addName}>
-        <input value={name}
-          onChange={handleNewName} />
+        Name: <input value={name}
+          onChange={handleNewName}
+          onClick={() => setName('')}/>
+        Number: <input value={number}
+          onChange={handleNewNumber}
+          onClick={() => setNumber('')} />
         <button type="submit">save</button>
       </form>
+      <Header text="Numbers" />
+      <ul>
+        {filteredPhonebook.map((phoneEntry, i) => {
+          return (
+            <div key={'div' + i}>
+              <li key={phoneEntry[0] + i}>{phoneEntry[0]} {phoneEntry[1]}</li>
+            </div>
+          )
+        })}
+      </ul>
     </main>
   )
 }
